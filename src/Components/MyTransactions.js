15df9979519648ebede28ed3a669175faf4d464b
Sarah Goewey <sarah.goewey@yahoo.com>
2023-08-16 16:44:57 -0400

    reordered transactions

--- src/Components/MyTransactions.js
+++ src/Components/MyTransactions.js
@@ -8,46 +8,56 @@ const MyTransactions = () => {
     <div>
       <h1>My Transactions</h1>
       <ul>
-        {transactions.map((trans) => {
-          return (
-            <li key={trans.id} className="transaction">
-              <p>Transaction # {trans.id}</p>
-              <p>Date: {trans.updatedAt.slice(0, 10)}</p>
-              <p>Total: {trans.total}</p>
-              <p>Tendered: {trans.tendered}</p>
-              <p>Change: {trans.change}</p>
-              <div className="dropdown">
-                <p>view items</p>
-                <div className="dropdown-content">
-                  <ul>
-                    {items
-                      .filter((item) => item.transactionId === trans.id)
-                      .map((item, idx) => {
-                        return (
-                          <li key={item.id} className="itemInTrans">
-                            <p>
-                              <b>item #{idx + 1}</b>
-                            </p>
-                            <p>name: {item.name}</p>
-                            <p>quantity: {item.quantity}</p>
-                            <p>price: {item.price}</p>
-                            <p>
-                              discount: {item.discount || "none"}
-                              {item.discount ? "%" : ""}
-                            </p>
-                            <p>
-                              tax:{" "}
-                              {item.taxState === "NY" ? "New York" : "none"}
-                            </p>
-                          </li>
-                        );
-                      })}
-                  </ul>
+        {transactions
+          .sort((a, b) => {
+            if (a.createdAt < b.createdAt) {
+              return 1;
+            }
+            if (a.createdAt > b.createdAt) {
+              return -1;
+            }
+            return 0;
+          })
+          .map((trans) => {
+            return (
+              <li key={trans.id} className="transaction">
+                <p>Transaction # {trans.id}</p>
+                <p>Date: {trans.updatedAt.slice(0, 10)}</p>
+                <p>Total: {trans.total}</p>
+                <p>Tendered: {trans.tendered}</p>
+                <p>Change: {trans.change}</p>
+                <div className="dropdown">
+                  <p>view items</p>
+                  <div className="dropdown-content">
+                    <ul>
+                      {items
+                        .filter((item) => item.transactionId === trans.id)
+                        .map((item, idx) => {
+                          return (
+                            <li key={item.id} className="itemInTrans">
+                              <p>
+                                <b>item #{idx + 1}</b>
+                              </p>
+                              <p>name: {item.name}</p>
+                              <p>quantity: {item.quantity}</p>
+                              <p>price: {item.price}</p>
+                              <p>
+                                discount: {item.discount || "none"}
+                                {item.discount ? "%" : ""}
+                              </p>
+                              <p>
+                                tax:{" "}
+                                {item.taxState === "NY" ? "New York" : "none"}
+                              </p>
+                            </li>
+                          );
+                        })}
+                    </ul>
+                  </div>
                 </div>
-              </div>
-            </li>
-          );
-        })}
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
